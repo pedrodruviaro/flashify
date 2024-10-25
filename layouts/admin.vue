8d5cbc5b5f1b8a03523d8d014fb4caa5ad0d399a
pedrodruviaro <pedrodruviaro@gmail.com>
2024-10-25 15:51:05 -0300

    feat: :tada: first commit and first config

---
+++ layouts/admin.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <p>>>> layout admin <<<</p>
+    <slot />
+  </div>
+</template>
---
+++ pages/dashboard/decks/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "admin",
+})
+</script>
+
+<template>
+  <div>
+    <p>Decks page</p>
+    <p>Aqui vai a listagem dos decks (ou blocos)</p>
+  </div>
+</template>
