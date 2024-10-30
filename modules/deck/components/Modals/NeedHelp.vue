2d1448a71d0ef9ca677a6442009917a738a15559
pedrodruviaro <pedrodruviaro@gmail.com>
2024-10-30 13:40:45 -0300

    feat: add context to need help banner on play screen

--- modules/deck/components/Modals/NeedHelp.vue
+++ modules/deck/components/Modals/NeedHelp.vue
@@ -11,7 +11,7 @@ const emits = defineEmits<{
     <UCard>
       <template #header>
         <div class="flex justify-between flex-wrap gap-2">
-          <BaseTitle size="xs" light label="Como funciona" />
+          <BaseTitle size="sm" light label="Como funciona" />
           <UButton
             color="gray"
             variant="ghost"
@@ -23,14 +23,21 @@ const emits = defineEmits<{
         </div>
       </template>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. In aut, ratione
-        nam culpa, sed error ipsum itaque tenetur sapiente minus officia. Harum
-        non itaque quis, necessitatibus ex facilis voluptatem similique a
-        incidunt esse voluptate pariatur, accusantium perspiciatis. Reiciendis,
-        soluta. Architecto natus accusantium corporis minus accusamus dolorem!
-        Recusandae vel incidunt voluptates.
-      </p>
+      <div class="space-y-2">
+        <p>
+          Flashcards são cartões que servem para testar sua memória. De um lado,
+          eles tem a pergunta, e do outro, a respostaa. É possível variar
+          também, com tópicos e palavras-chave ou termos e definições.
+        </p>
+        <p>
+          Clique nos cards e veja se a resposta bate com a sua. Marque o
+          resultado, conte os acertos e veja seu desempenho.
+        </p>
+        <p>
+          Você pode ver as perguntas respondidas no menu central. Em verde ficam
+          as respostas certas e em vermelho, as erradas.
+        </p>
+      </div>
     </UCard>
   </UModal>
 </template>
